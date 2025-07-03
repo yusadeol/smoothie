e1891c38f50eaa197791eedc9adecabf241cc01b
Yuri Oliveira <yusadeol@gmail.com>
2025-07-03 09:42:42 -0300

    fix: update basePath to check for non string path
    The function now returns the base path if path is not a string,
    empty string, or '0'.

--- app/helpers.php
+++ app/helpers.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 function basePath(?string $path = null): string
 {
     $basePath = dirname(__DIR__);
-    if ($path === null || $path === '' || $path === '0') {
+    if (! is_string($path) || ($path === '' || $path === '0')) {
         return $basePath;
     }
 
